8afdf14d7214ce611366c229776bcb1a96bb33e1
Mitko Nikolov <mitko.nikoloff@gmail.com>
2014-06-13 23:33:30 +0300

    Added info to data.js, added some code to loader.js
    The information about celestial objects is in the object 'data'. In
    data.info there is a short summary about the planet form Wikipedia. The
    other properties are meant to make it easier to make something like
    .toString() for the planets if we ever need it.
    Added construction of the orbits and space objects in the loader.js
    (with a suggestion constructor for the space objects).
    Comments in bulgarian are not meant to stay in the code.

--- SolarSystem/scripts/loader.js
+++ SolarSystem/scripts/loader.js
@@ -1,43 +1,69 @@
 /*global CelestialBody, Orbit */
 
-//  var loader = new Loader(Data.collection, stage.width(), stage.height());
-//  var selestialObjects = loader.load();
+// To get the objects from Data.collection as space objects (or celestial objects, or whatever) use:
+// var loader = new Loader(Data.collection, centerX, centerY);
+// var selestialObjects = loader.load();
+// Where centerX and centerY are the center of the container (the stage)
+// ... i.e. centerX = stage.width() / 2, centerY = stage.height() / 2
 
-// var Loader = function (collection, centerX, centerY) {
-var Loader = function (collection, containerWidth, containerHeight) {
+// var Loader = function (collection, containerWidth, containerHeight) {
+var Loader = function (collection, centerX, centerY) {
     'use strict';
     this.load = function () {
         var celestialBodies = [],
             length = collection.length,
             celestialBody,
             orbit,
-            i;
+            i,
+            orbitCenterX,
+            orbitCenterY,
+            orbitRadiusX,
+            orbitRadiusY,
+            name,
+            radius,
+            angularSpeed,
+            orbitSpeed;
 
         for (i = 0; i < length; i += 1) {
-            orbit = new Orbit();
-            celestialBody = new CelestialBody(/*...,*/ orbit); // ?? or whatever it will be called. It should describe both the planets and the Sun
-            // Properties to use:
-            // x -> centerX or containerWidth / 2
-            // y -> centerX or containerHeight / 2
-            // collection[i].name
-            // collection[i].radius
-            // collection[i].angularSpeed
-            // collection[i].imgSrc
-            // collection[i].orbitSpeed
-            // collection[i].orbitOffset
-            // \/ for cicle \/
-            // collection[i].orbitRadius
-            // \/ for elipse \/
-            // collection[i].orbitRadiusX
-            // collection[i].orbitRadiusY
-            // + For orbit:
-            //      period = 10000 / speed
-            //      stroke = 'white';
-            //      stroke width = 0.2;
-            // solarSystemObject.imgSrc = collection[i].imgSrc;
+            orbitCenterX = centerX + collection[i].orbitOffset.x;
+            orbitCenterY = centerY + collection[i].orbitOffset.y;
+            orbitRadiusX = collection[i].orbitRadiusX;
+            orbitRadiusY = collection[i].orbitRadiusY;
+
+            orbit = new Orbit(orbitCenterX, orbitCenterY, orbitRadiusX, orbitRadiusY);
+
+            name = collection[i].name;
+            radius = collection[i].radius;
+            angularSpeed = collection[i].angularSpeed;
+            orbitSpeed = collection[i].orbitSpeed;
+
+            // Примерен конструктор за планетите и слънцето:
+            celestialBody = new CelestialBody(name, radius, angularSpeed, orbitSpeed, orbit);
+            // х и у могат да се вземaт от орбитата
+            // периода - от скоростта
+            // офсетa за картинката - от радиуса
+
+            celestialBody.imgSrc = collection[i].imgSrc;
+            // В 'celestialBody.imgSrc', 'imgSrc' трябва да се прекръсти на това, което ще бъде за картинката
+            // в планетата, ако е различно.
+
+            // Data is in this format to make it easier to make a .toString() method for the planets in which the info
+            // can be stiched together any way we see fit (if that's ever needed).
+            // We can otherwise just use celestialBody.data.info in which there is some Wikipedia text.
+            celestialBody.data = {
+                radius: collection[i].data.radius,
+                dayLength: collection[i].data.dayLength,
+                closestToSun: collection[i].data.closestToSun,
+                farthestFromSun: collection[i].data.farthestFromSun,
+                timeToOrbitSun: collection[i].data.timeToOrbitSun,
+                info: collection[i].data.info,
+            };
+
             celestialBodies.push(celestialBody);
         }
 
         return celestialBodies;
+        // celestialBodies[0] -> the Sun
+        // celestialBodies[n != 0] -> the planets
     };
 };
